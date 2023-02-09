b707c3d3976a3198275c985281149f1f231509a3
fgriff <anglirum@gmail.com>
2023-02-09 19:47:22 +0300

    refactor: add name into logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 import style from './Logo.scss';
 import logoIcon from '../../assets/img/svg/logo.svg';
+import { useTypedSelector } from '../../redux/hooks';
 
 const Logo = () => {
+  const { isLightTheme } = useTypedSelector(({ common }) => common);
+  const themeClass = isLightTheme ? style.logo_light : style.logo_dark;
+
   return (
     <Link
       to="/"
-      className={style.logo}
+      className={classNames(style.logo, themeClass)}
     >
       <img
+        className={style.logo__icon}
         src={logoIcon}
         alt="Logo"
       />
+      <span className={style.logo__name}>вконтакте</span>
     </Link>
   );
 };

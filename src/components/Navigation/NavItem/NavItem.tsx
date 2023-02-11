585a522a2cd2a881b51a3e34fb9310fdaa7ce65e
fgriff <anglirum@gmail.com>
2023-02-11 21:03:19 +0300

    fix: logo font

---
+++ src/components/Navigation/NavItem/NavItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Nav.scss';
+
+const Nav = () => {
+  return (
+    <nav className={style.navigation}>
+
+    </nav>
+  );
+};
+
+export default Nav;

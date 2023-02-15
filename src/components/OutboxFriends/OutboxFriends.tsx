4e2c4a39d91bdf9ae369859643ff65c47ce94748
fgriff <anglirum@gmail.com>
2023-02-15 20:03:14 +0300

    feat: add friend card

--- src/components/OutboxFriends/OutboxFriends.tsx
+++ src/components/OutboxFriends/OutboxFriends.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import style from './OutboxFriends.scss';
+import FriendCard from '../FriendCard/FriendCard';
 
 const OutboxFriends = () => {
-  return <div className={style.outbox}></div>;
+  return (
+    <div className={style.outbox}>
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+    </div>
+  );
 };
 
 export default OutboxFriends;

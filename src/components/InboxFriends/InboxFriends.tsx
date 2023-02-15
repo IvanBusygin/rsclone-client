5920994419c411912cf2556e9679a8708ff7ec08
fgriff <anglirum@gmail.com>
2023-02-15 17:49:47 +0300

    feat: add search friends form and friends page component

---
+++ src/components/InboxFriends/InboxFriends.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './InboxFriends.scss';
+
+const InboxFriends = () => {
+  return <div className={style.inbox}></div>;
+};
+
+export default InboxFriends;
---
+++ src/components/OutboxFriends/OutboxFriends.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './OutboxFriends.scss';
+
+const OutboxFriends = () => {
+  return <div className={style.outbox}></div>;
+};
+
+export default OutboxFriends;

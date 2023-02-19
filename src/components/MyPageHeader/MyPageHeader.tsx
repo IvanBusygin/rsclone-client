0ea29861e6405090e29114c3d6e1835a5c352e44
fgriff <anglirum@gmail.com>
2023-02-19 21:41:14 +0300

    feat: add user info to "my page"

--- src/components/MyPageHeader/MyPageHeader.tsx
+++ src/components/MyPageHeader/MyPageHeader.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames';
 import style from './MyPageHeader.scss';
-import { useTypedSelector } from '../../redux/hooks';
+import { useTypedDispatch, useTypedSelector } from '../../redux/hooks';
 import userDefaultAvatar from '../../assets/img/svg/user_default_icon.svg';
+import { getUserInfo } from '../../redux/thunks';
 
 const MyPageHeader = () => {
   const { isLightTheme } = useTypedSelector(({ common }) => common);
   const themeClass = isLightTheme ? style.myPage__header_light : style.myPage__header_dark;
+
+  const { infoData } = useTypedSelector(({ editPage }) => editPage);
+
+  const dispatch = useTypedDispatch();
+
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, [dispatch]);
 
   return (
     <section className={classNames(style.myPage__header, themeClass)}>
@@ -19,14 +28,14 @@ const MyPageHeader = () => {
       <div className={style.myPage__userInfo}>
         <div className={style.myPage__avatar}>
           <img
-            src={userDefaultAvatar}
+            src={infoData.avatar || userDefaultAvatar}
             alt="Avatar"
             className={style.myPage__image}
           />
         </div>
         <div className={style.myPage__info}>
-          <h2 className={style.myPage__name}>Петя Камушкин</h2>
-          <p className={style.myPage__status}>{`I'm best of the best`}</p>
+          <h2 className={style.myPage__name}>{`${infoData.firstName} ${infoData.lastName}`}</h2>
+          <p className={style.myPage__status}>{infoData.status}</p>
         </div>
       </div>
     </section>

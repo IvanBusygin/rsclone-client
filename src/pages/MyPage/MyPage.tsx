1e4e87e9b46b9c86657567e193acfd37d368f2e5
fgriff <anglirum@gmail.com>
2023-02-22 13:52:14 +0300

    refactor: move getting user info from MyPageHeader to MyPage

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './MyPage.scss';
 import MyPageHeader from '../../components/MyPageHeader/MyPageHeader';
 import Wall from '../../components/Wall/Wall';
 import FriendIconList from '../../components/FriendIconList/FriendIconList';
 import EditButton from '../../components/EditButton/EditButton';
+import { useTypedDispatch } from '../../redux/hooks';
+import { getUserInfo } from '../../redux/thunks/editPageThunks';
 
 const MyPage = () => {
+  const dispatch = useTypedDispatch();
+
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, [dispatch]);
+
   return (
     <div className={style.myPage}>
       <MyPageHeader />

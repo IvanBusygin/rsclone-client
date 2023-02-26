19608c798daa106e2c3b0eaa7ab5e0718a81dc5c
IvanBusygin <bi-25@yandex.ru>
2023-02-27 00:31:21 +0400

    feat: ButtonAddToFriend

---
+++ src/components/friends/ButtonAddToFriend/ButtonAddToFriend.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Preloader from '../../Preloader/Preloader';
+import { fetchAddFriend } from '../../../redux/slices/friendsSlice';
+import { useTypedDispatch, useTypedSelector } from '../../../redux/hooks';
+import style from './ButtonAddToFriend.scss';
+
+interface IProps {
+  id: string;
+}
+
+function ButtonAddToFriend(props: IProps) {
+  const { id } = props;
+
+  const dispatch = useTypedDispatch();
+  const { loadingAdd } = useTypedSelector(({ friends }) => friends);
+
+  const [stateLoading, setStateLoading] = useState(false);
+
+  useEffect(() => {
+    if (loadingAdd === false) setStateLoading(false);
+  }, [loadingAdd]);
+
+  const addHandler = () => {
+    setStateLoading(true);
+    dispatch(fetchAddFriend(id));
+  };
+
+  return (
+    <button
+      className={style.button}
+      type="button"
+      onClick={addHandler}
+    >
+      {stateLoading && loadingAdd ? <Preloader /> : 'Добавить в друзья'}
+    </button>
+  );
+}
+
+export default ButtonAddToFriend;
--- src/components/friends/FriendCards/FriendCardFound.tsx
+++ src/components/friends/FriendCards/FriendCardFound.tsx
@@ -1,31 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import style from './FriendCardFound.scss';
-import { useTypedDispatch, useTypedSelector } from '../../../redux/hooks';
+import { useTypedSelector } from '../../../redux/hooks';
 import { IDataPeople } from '../../../types/friends';
-import { fetchAddFriend } from '../../../redux/slices/friendsSlice';
 import userDefaultAvatar from '../../../assets/img/svg/user_default_icon.svg';
-import Preloader from '../../Preloader/Preloader';
+import ButtonAddToFriend from '../ButtonAddToFriend/ButtonAddToFriend';
 
 const FriendCardFound = (props: IDataPeople) => {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const { _id, info } = props;
   const { isLightTheme } = useTypedSelector(({ common }) => common);
   const themeClass = isLightTheme ? style.friendsPage_light : style.friendsPage_dark;
-
-  const dispatch = useTypedDispatch();
-  const { loadingAdd } = useTypedSelector(({ friends }) => friends);
-
-  const [stateLoading, setStateLoading] = useState(false);
-
-  useEffect(() => {
-    if (loadingAdd === false) setStateLoading(false);
-  }, [loadingAdd]);
-
-  const addHandler = () => {
-    setStateLoading(true);
-    dispatch(fetchAddFriend(_id));
-  };
 
   return (
     <div className={classNames(style.friendCard, themeClass)}>
@@ -41,13 +26,7 @@ const FriendCardFound = (props: IDataPeople) => {
           <p className={style.friendCard__name}>{info.fullName}</p>
         </div>
       </div>
-      <button
-        className={style.friendCard__btn}
-        type="button"
-        onClick={addHandler}
-      >
-        {stateLoading && loadingAdd ? <Preloader /> : 'Добавить в друзья'}
-      </button>
+      <ButtonAddToFriend id={_id} />
     </div>
   );
 };

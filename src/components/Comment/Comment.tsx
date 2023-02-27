9356f672130b343f73ed389efd8f11fe5ad272e0
fgriff <anglirum@gmail.com>
2023-02-28 01:43:35 +0300

    feat: add light and dark themes for comment

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,14 +1,19 @@
 import React, { FC } from 'react';
+import classNames from 'classnames';
 import style from './Comment.scss';
 import getLocaleTimeString from '../../utils/myPage';
 import userDefaultAvatar from '../../assets/img/svg/user_default_icon.svg';
 import { ICommentProps } from '../../types/comment';
+import { useTypedSelector } from '../../redux/hooks';
 
 const Comment: FC<ICommentProps> = (props) => {
   const { avatar, date, fullName, text } = props;
 
+  const { isLightTheme } = useTypedSelector(({ common }) => common);
+  const themeClass = isLightTheme ? style.comment_light : style.comment_dark;
+
   return (
-    <div className={style.comment}>
+    <div className={classNames(style.comment, themeClass)}>
       <header className={style.comment__header}>
         <div className={style.comment__avatar}>
           <img

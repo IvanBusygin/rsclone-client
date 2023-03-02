969ba4b9d363e13c83d858f5497f7cb4a8ac1eb0
IvanBusygin <bi-25@yandex.ru>
2023-03-02 16:18:31 +0400

    feat: add small friends icons to myPage

--- src/components/FriendIcon/FriendIcon.tsx
+++ src/components/FriendIcon/FriendIcon.tsx
@@ -1,20 +1,25 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 import style from './FriendIcon.scss';
 import { IFriendProps } from '../../types/myPage';
+import userDefaultAvatar from '../../assets/img/svg/user_default_icon.svg';
 
 const FriendIcon: FC<IFriendProps> = (props) => {
-  const { image, name } = props;
+  const { image, name, friendId } = props;
 
   return (
-    <div className={style.friend}>
-      <div className={style.friend__avatar}>
-        <img
-          src={image}
-          alt="FriendIcon avatar"
-        />
-      </div>
+    <Link
+      to={`/friend/${friendId}`}
+      target="_blank"
+      className={style.friend}
+    >
+      <img
+        className={style.friend__avatar}
+        src={image || userDefaultAvatar}
+        alt="avatar"
+      />
       <span className={style.friend__name}>{name.split(' ')[0]}</span>
-    </div>
+    </Link>
   );
 };
 

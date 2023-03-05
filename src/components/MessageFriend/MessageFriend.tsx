8c3d825086efc47a56d8763a51d867f540f3ea1f
fgriff <anglirum@gmail.com>
2023-03-06 01:02:43 +0300

    feat: add message friend component

---
+++ src/components/MessageFriend/MessageFriend.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import style from './MessageFriend.scss';
+import { IMessengerFriendProps } from '../../types/messenger';
+
+const MessageFriend: FC<IMessengerFriendProps> = (props) => {
+  const { id, avatar, fullName, indicator, onFriendClick } = props;
+
+  return (
+    <button
+      className={style.messageFriend__button}
+      type="button"
+      onClick={() => onFriendClick(id)}
+    >
+      <div className={style.messageFriend__avatar}>
+        <img
+          src={avatar}
+          alt="friend avatar"
+        />
+      </div>
+      <p className={style.messageFriend__name}>{fullName}</p>
+      {indicator === id && <span className={style.messageFriend__indicator} />}
+    </button>
+  );
+};
+
+export default MessageFriend;
